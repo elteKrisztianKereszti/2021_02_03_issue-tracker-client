4c3bd8e605b1a07408c9e103690b3910c5b28ee0
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-08 09:45:57 +0200

    add index component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { MyComponentComponent } from './components/my-component/my-component.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent
+    MyComponentComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
