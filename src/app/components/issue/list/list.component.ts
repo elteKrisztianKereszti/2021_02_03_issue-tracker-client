b98e3ca6ae4a0fe134e47f79cb48ab2028780ac2
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-22 09:40:22 +0200

    add list for issues (dynamic data)

--- src/app/components/issue/list/list.component.ts
+++ src/app/components/issue/list/list.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Issue } from 'src/app/issue';
 
 @Component({
   selector: 'app-issue-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent {
+  public issues: Array<Issue> = [
+    {
+      id: 1,
+      title: 'Bad Machine',
+      description: 'Bad Machine in the Farm',
+      status: 'NEW',
+      place: 'PC01'
+    },
+    {
+      id: 2,
+      title: 'Bad Mouse',
+      description: 'Mickey Mouse ill',
+      status: 'NEW',
+      place: 'PC03'
+    },
+    {
+      id: 3,
+      title: 'Jim Raynor angry',
+      description: 'Cannot run Stracraft on 386 arch computer',
+      status: 'DOING',
+      place: 'PC66'
+    },
+    {
+      id: 4,
+      title: 'No power',
+      description: 'NO power in PC42',
+      status: 'DONE',
+      place: 'PC42'
+    },
+  ];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
