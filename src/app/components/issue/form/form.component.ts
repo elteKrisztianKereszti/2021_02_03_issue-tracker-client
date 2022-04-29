09674a24ba3b8bbad2f252713a15f062a7da2fbc
kereszti.krisztian <kereszti.krisztian@marktsoft.hu>
2022-04-29 09:43:05 +0200

    update issueform to reactive froms with validations

--- src/app/components/issue/form/form.component.ts
+++ src/app/components/issue/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Issue } from 'src/app/issue';
 
 @Component({
@@ -10,10 +10,26 @@ import { Issue } from 'src/app/issue';
 export class FormComponent implements OnInit {
   @Input() issue: Issue;
 
-  constructor() {
+  public issueForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.issueForm = this.formBuilder.group({
+      title: [ this.issue.title, [ Validators.required ]],
+      description: this.issue.description,
+      place: [ this.issue.place, [ Validators.required ]],
+      status: [ this.issue.status, [ Validators.required ]],
+    });
   }
 
+  public onSubmit(e: any) {
+    e.preventDefault();
+    console.log(this.issueForm);
+  }
+
+  public setIsInvalidClass(property: string) {
+    return this.issueForm.get(property)?.invalid && (this.issueForm.get(property)?.dirty || this.issueForm.get(property)?.touched)
+  }
 }
